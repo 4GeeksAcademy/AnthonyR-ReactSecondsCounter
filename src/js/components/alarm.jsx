9bec0f141836da850c50aa65e0de85f2f96cbb6c
Tonyreng <tonyreng01@gmail.com>
2025-04-21 02:45:46 +0000

    Corrigiendo problema de id duplicado

--- src/js/components/alarm.jsx
+++ src/js/components/alarm.jsx
@@ -4,10 +4,10 @@ import React from "react";
 export const Alarm = (props) => {
 
     return (
-        <div className="row text-white" style={{background: 'black'}}>
+        <div className="row text-white" style={{ background: 'black' }}>
             <div className="mb-3 container text-center col-2">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Ingresa para crear tu alarma</label>
-                <input type="text" className="form-control text-center" id="exampleFormControlInput1" placeholder="Tu alarma" onChange={props.alarm} />
+                <label htmlFor="exampleFormControlInput2" className="form-label">Ingresa para crear tu alarma</label>
+                <input type="text" className="form-control text-center" id="exampleFormControlInput2" placeholder="Tu alarma" onChange={props.alarm} />
             </div>
         </div>
     );
